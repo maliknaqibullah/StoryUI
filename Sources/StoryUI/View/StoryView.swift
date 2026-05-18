de1c7d77fc43065d63136ad81b10ac4d3766f9f5
mrrhim610-hue <mrrhim610@gmail.com>
2026-05-18 11:56:19 +0430

    loading skelton redesigned

--- Sources/StoryUI/View/StoryView.swift
+++ Sources/StoryUI/View/StoryView.swift
@@ -56,6 +56,7 @@ public struct StoryView: View {
                             onDeleteTapped: onDeleteTapped,
                             myUserID: myUserID
                         )
+                        .tag(model.id)
                     }
                 }
             }
